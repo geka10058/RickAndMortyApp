e1779a31846b011c414672cfcb5daa159b475809
Kirychenka Yauheni <geka10058@gmail.com>
2023-03-05 14:29:19 +0300

    First homework assignment completed
    add:
    - New color scheme
    - Launcher icon
    - Custom font
    - Images for splashScreen
    - Theme for splashScreen

--- app/src/main/java/com/example/rickmortyapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rickmortyapp/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.rickmortyapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -11,10 +12,11 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed({
-            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            startActivity(i)
+
+        val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(intent)
             finish()
-        }, 3000)
+        }, 2000)
     }
 }
